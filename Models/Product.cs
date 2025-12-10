a995433180c63abaa05754cc1356f989a30573bc
Camila Paschini <camila.paschini@gmail.com>
2025-12-10 10:42:09 +0100

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30, ErrorMessage = "Produc.Name must be at least 5 and no more than 30 characters"), MinLength(5)]
+        public string Name { get; set; } = string.Empty;
+        public int Price { get; set; } = 10;
+
+        [DataType(DataType.Date)]
+        public DateTime? OrderDate { get; set; } = DateTime.Now;
+        public string Category { get; set; } = "Övrigt";
+        public string Shelf { get; set; } = "111";
+        public int Count { get; set; } = 0;
+
+        [MaxLength(150)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Storage.Data;
+namespace Storage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<StorageContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("StorageContext") ?? throw new InvalidOperationException("Connection string 'StorageContext' not found.")));
+                // configuration string in appsettings.jsonm
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                //pattern: "{controller=Home}/{action=Index}/{id?}")
+                pattern: "{controller=Products}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
